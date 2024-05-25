96c9791bc161bd8d1e0eea69d78f3ee66a182bea
bigboykirusha <105214100+bigboykirusha@users.noreply.github.com>
2024-05-26 00:25:47 +0300

    123

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -34,18 +34,18 @@ VAT ID: TH0123456789`,
       description: `Phone: +66 (0)2 987 6543<br />
 Hours: Monday to Friday, 9am to 5pm (ICT)`,
     },
-];
+  ];
 
 
 
 
-  return (
+  return (<div className={styles.root}>
     <div className={styles.contacts}>
       <div className={styles.contacts__map}>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5959.601588084356!2d44.827630445770254!3d41.681645466144275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40440c44cc4ebe8b%3A0xa5d8f7c6f612009!2sTBC%20Bank!5e0!3m2!1sen!2sge!4v1716664545950!5m2!1sen!2sge" loading="lazy"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5959.601588084356!2d44.827630445770254!3d41.681645466144275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40440c44cc4ebe8b%3A0xa5d8f7c6f612009!2sTBC%20Bank!5e0!3m2!1sen!2sge!4v1716664545950!5m2!1sen!2sge" loading="lazy"></iframe>
       </div>
       <div className={styles.contacts__data}>
-      <div className={styles.contacts__title}>Smart Greenhouses Co., Ltd.</div>
+        <div className={styles.contacts__title}>Smart Greenhouses Co., Ltd.</div>
         <div className={styles.contacts__cardsContainer}>
           {officeData.map((card, index) => (
             <ContactsCard
@@ -57,6 +57,7 @@ Hours: Monday to Friday, 9am to 5pm (ICT)`,
         </div>
       </div>
     </div>
+  </div>
   );
 };
 
